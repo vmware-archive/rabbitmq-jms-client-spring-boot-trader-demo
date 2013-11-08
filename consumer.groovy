3ccecfb7dd324fa4cb1405e87a8140bef679c8fd
Steve Powell <steve@rabbitmq.com>
2013-11-08 15:00:14 +0000

    Create 1.1.1 release

--- consumer.groovy
+++ consumer.groovy
@@ -1,7 +1,7 @@
 package com.rabbitmq.jms.sample
 
 @GrabResolver(name="vmware-snapshot", root="http://private.maven.vmware.com.s3.amazonaws.com/snapshot")
-@Grab("com.rabbitmq.jms:rabbitmq-jms:1.1.1-SNAPSHOT")
+@Grab("com.rabbitmq.jms:rabbitmq-jms:1.1.1")
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory
 
