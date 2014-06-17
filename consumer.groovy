ce96419d4d613e5169ebdf4b1e857fdf16a0a087
Steve Powell <steve@rabbitmq.com>
2014-06-17 16:25:23 +0100

    RJMS-1.2.3

--- consumer.groovy
+++ consumer.groovy
@@ -1,6 +1,6 @@
 package com.rabbitmq.jms.sample
 
-@Grab("com.rabbitmq.jms:rabbitmq-jms:1.2.3-SNAPSHOT")
+@Grab("com.rabbitmq.jms:rabbitmq-jms:1.2.3")
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory
 
