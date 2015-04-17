06b1d90cfef6fe89dfe3892a2787f1c4c387575e
Steve Powell <spowell@pivotal.io>
2015-04-17 17:22:11 +0100

    RJMS-1.4.1

--- consumer.groovy
+++ consumer.groovy
@@ -1,6 +1,6 @@
 package com.rabbitmq.jms.sample
 
-@Grab("com.rabbitmq.jms:rabbitmq-jms:1.4.1-SNAPSHOT")
+@Grab("com.rabbitmq.jms:rabbitmq-jms:1.4.1")
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory
 
